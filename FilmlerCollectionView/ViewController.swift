bc49460f865ed219c955333495fc5bec2b46fc67
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-09 00:36:05 +0300

    Initial Commit

---
+++ FilmlerCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmlerCollectionView
+//
+//  Created by Ömer on 9.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FilmlerCollectionViewTests/FilmlerCollectionViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  FilmlerCollectionViewTests.swift
+//  FilmlerCollectionViewTests
+//
+//  Created by Ömer on 9.04.2025.
+//
+
+import Testing
+@testable import FilmlerCollectionView
+
+struct FilmlerCollectionViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
