3976753efea256901e8a46852ad199029d4c0d91
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-09 22:09:52 +0300

    Filmler sınıfı oluşturuldu

--- FilmlerCollectionView/CollectionViewCell.swift
+++ FilmlerCollectionView/CollectionViewCell.swift
@@ -9,4 +9,10 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var fiyatLabel: UILabel!
+    @IBOutlet weak var isimLabel: UILabel!
+    @IBOutlet weak var filmImage: UIImageView!
+    
+    @IBAction func tikla(_ sender: Any) {
+    }
 }
--- FilmlerCollectionView/ViewController.swift
+++ FilmlerCollectionView/ViewController.swift
@@ -8,10 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var filmlerListesi = [Filmler]()
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let f1 = Filmler(filmId: 1, filmBaslik: "Django", filmResimAdi: "django", filmFiyat: 15.99)
+        let f2 = Filmler(filmId: 2, filmBaslik: "Inception", filmResimAdi: "inception", filmFiyat: 15.99)
+        let f3 = Filmler(filmId: 3, filmBaslik: "Interstellar", filmResimAdi: "interstellar", filmFiyat: 15.99)
+        let f4 = Filmler(filmId: 4, filmBaslik: "Anadoluda", filmResimAdi: "birzamanlaranadoluda", filmFiyat: 15.99)
+        let f5 = Filmler(filmId: 5, filmBaslik: "The Hateful Eight", filmResimAdi: "thehatefuleight", filmFiyat: 15.99)
+        let f6 = Filmler(filmId: 6, filmBaslik: "The Pianist", filmResimAdi: "thepianist", filmFiyat: 15.99)
+        
+        filmlerListesi.append(f1)
+        filmlerListesi.append(f2)
+        filmlerListesi.append(f3)
+        filmlerListesi.append(f4)
+        filmlerListesi.append(f5)
+        filmlerListesi.append(f6)
     }
 
 
