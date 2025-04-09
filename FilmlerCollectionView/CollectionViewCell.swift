27e6424555b5b3997afc78c1b37d8f977f62de67
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-09 22:17:56 +0300

    Collection oluşturuldu

--- FilmlerCollectionView/CollectionViewCell.swift
+++ FilmlerCollectionView/CollectionViewCell.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
+protocol FilmHucreProtocol {
+    func sepeteEkle(indexPath:IndexPath)
+}
+
+
 class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var fiyatLabel: UILabel!
     @IBOutlet weak var isimLabel: UILabel!
     @IBOutlet weak var filmImage: UIImageView!
     
+    var hucreProtocol:FilmHucreProtocol?
+    var indexPath:IndexPath?
+    
     @IBAction func tikla(_ sender: Any) {
+        
+        hucreProtocol?.sepeteEkle(indexPath: indexPath!)
     }
 }
--- FilmlerCollectionView/ViewController.swift
+++ FilmlerCollectionView/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate,FilmHucreProtocol{
     
     var filmlerListesi = [Filmler]()
 
@@ -31,6 +31,38 @@ class ViewController: UIViewController {
         filmlerListesi.append(f5)
         filmlerListesi.append(f6)
     }
+    
+    
+    func sepeteEkle(indexPath: IndexPath) {
+        let film = filmlerListesi[indexPath.row]
+        print("\(film.filmBaslik!) filmi sipariş verildi.")
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filmlerListesi.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let film = filmlerListesi[indexPath.row]
+        
+        let cell  = collectionView.dequeueReusableCell(withReuseIdentifier: "filmCol", for: indexPath) as! CollectionViewCell
+        
+        cell.isimLabel.text = film.filmBaslik!
+        cell.fiyatLabel.text = "\(film.filmFiyat!) TL"
+        cell.filmImage.image = UIImage(named:film.filmResimAdi!)
+        
+        cell.hucreProtocol = self
+        cell.indexPath = indexPath
+        
+   
+        return cell
+    }
+    
 
 
 }
