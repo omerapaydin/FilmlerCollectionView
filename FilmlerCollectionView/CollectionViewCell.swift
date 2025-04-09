509ecec60bb3780f3b26c523b97f9e3ad51e71e8
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-09 22:09:32 +0300

    Filmler sınıfı oluşturuldu

---
+++ FilmlerCollectionView/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  FilmlerCollectionView
+//
+//  Created by Ömer on 9.04.2025.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ FilmlerCollectionView/Filmler.swift
@@ -0,0 +1,8 @@
+//
+//  Filmler.swift
+//  FilmlerCollectionView
+//
+//  Created by Ömer on 9.04.2025.
+//
+
+import Foundation
